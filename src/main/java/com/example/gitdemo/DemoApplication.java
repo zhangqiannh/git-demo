addfe8a0966b5d17ae773dc064e3a2e09269f488
zhangqian <zhangqiannh@163.com>
2020-02-29 21:49:34 +0800

    update by king2

--- src/main/java/com/example/gitdemo/DemoApplication.java
+++ src/main/java/com/example/gitdemo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaa");
-		System.out.println("bbbbbbbbbbb");
+		System.out.println("bbbbbbbbbbib");
+		System.out.println("bbbbbbbbbbib");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
