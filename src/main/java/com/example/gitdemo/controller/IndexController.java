4107418ac0a02358b38c837f5ba35d743fb642ee
zhangqian <zhangqiannh@163.com>
2020-02-29 15:28:32 +0800

    add IndexConroller in branch kings

---
+++ src/main/java/com/example/gitdemo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * IndexController
+ */
+
+@RestController
+public class IndexController {
+
+  @GetMapping("/index")
+  public String index() {
+      return "this is gitdemo";
+  }
+}
