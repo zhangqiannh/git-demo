a4a6683d1451fab6f744a52bbf202d9d251cd2e3
zhangqian <zhangqiannh@163.com>
2020-02-29 21:27:51 +0800

    test clone commit

--- src/main/java/com/example/gitdemo/DemoApplication.java
+++ src/main/java/com/example/gitdemo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaa");
+		System.out.println("bbbbbbbbbbb");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
